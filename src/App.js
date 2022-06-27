706ddf02defc4eeccfc2a143076950f849964224
ÇETİN <cetinnnarasss@gmail.com>
2022-06-27 21:23:19 +0300

    cont.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import Card from "./components/card/Card";
-import data from "./util/data";
+import Events from './components/1-event-intro/Events';
 
 function App() {
-  //? console.log(data);
   return (
-    <>
-    {data.map((item) => {
-      //!Destruction(ayırma) ,,map = dolaşma
-      const {id, img, btnName, dil} = item;
-      
-      return <Card key={id} img={img} btnName={btnName} dil={dil}/>
-      //key burda uniq bir değer ver ki hata vermesin.
-      //id yerine idex de kullanılabılır ama ıd kullanalım(mapde index kullanabılırsın)
-
-    
-    })}
-      
-    </>
+    <div>
+      <Events />
+    </div>
   );
 }
-
 export default App;
